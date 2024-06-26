35a38e9fbf438cc3749b8621dda828f56f334a00
tparsons <tparsons@godaddy.com>
2024-06-25 20:50:01 -0700

    fixed string error

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  site: 'https://tims-newsletter.pages.github.io',
 });
